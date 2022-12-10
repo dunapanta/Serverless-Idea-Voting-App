b463d0bc8dddfba86a6da5af85ea552758010f33
Daniel Unapanta <dunapanta@outlook.es>
2022-12-10 00:08:15 -0500

    Add createIdea

---
+++ src/functions/createIdea/index.ts
@@ -0,0 +1,50 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { v4 as uuid } from 'uuid';
+
+import { formatJSONResponse } from '@libs/APIResponses';
+import Dynamo from '@libs/Dynamo';
+import { CreateIdeaBody } from 'src/types/apiTypes';
+import { IdeaRecord } from 'src/types/dynamo';
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+  try {
+    const body = JSON.parse(event.body!);
+    const tableName = process.env.singleTable;
+
+    const validationError = validateBody(body);
+    if (validationError) {
+      return validationError;
+    }
+
+    const { title, description, boardId } = body as CreateIdeaBody;
+
+    const data: IdeaRecord = {
+      id: uuid(),
+      pk: `idea-${boardId}`,
+      sk: Date.now().toString(),
+
+      boardId,
+      ideaTitle: title,
+      description,
+      date: Date.now(),
+    };
+
+    await Dynamo.write({ data, tableName });
+
+    return formatJSONResponse({ body: { message: 'Idea Created', id: data.id } });
+  } catch (error) {
+    return formatJSONResponse({ statusCode: 500, body: error.message });
+  }
+};
+
+const validateBody = (body: Record<string, any>) => {
+  const { title, boardId } = body;
+
+  if (!title || !boardId) {
+    return formatJSONResponse({
+      statusCode: 400,
+      body: '"title and "boardId are required',
+    });
+  }
+  return;
+};
